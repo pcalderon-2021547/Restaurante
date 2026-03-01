fef659f31a956e3136e3a59e949f41a48b2282f1
mlara-2021019 <mlara-2021019@kinal.edu.gt>
2026-03-01 04:14:13 -0600

    add: agregando comentarios de routes para su mejor comprensión

--- src/fields/reservation/reservationRoutes.js
+++ src/fields/reservation/reservationRoutes.js
@@ -6,24 +6,29 @@ import { requireRole } from '../../../middlewares/validate_role.js';
 
 const router = Router();
 
+//crear
 router.post(
     '/create', validateJWT,
     requireRole('ADMIN_ROLE'),createReservation
 );
+//listar
 router.get(
     '/', getReservations
 );
+//listar por fecha
 router.get(
     '/by-date', getReservationsByDate
 );
+//listar por Id
 router.get(
-
     '/:id', getReservationById
 );
+//actualizar
 router.put(
     '/update/:id',validateJWT,
     requireRole('ADMIN_ROLE'), updateReservation
 );
+//eliminar
 router.delete(
     '/delete/:id',validateJWT,
     requireRole('ADMIN_ROLE'), deleteReservation
