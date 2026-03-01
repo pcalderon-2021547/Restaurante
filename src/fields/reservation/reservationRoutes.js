316791cadc779532dd9fa87c3edce1c9da1c18b0
mlara-2021019 <mlara-2021019@kinal.edu.gt>
2026-03-01 00:16:10 -0600

    feat: rediseñando la estructura de reservationRoutes.js

--- src/fields/reservation/reservationRoutes.js
+++ src/fields/reservation/reservationRoutes.js
@@ -5,13 +5,23 @@ import { requireRole } from '../../../middlewares/validate_role.js';
 
 const router = Router();
 
-router.post('/create', validateJWT,
-    requireRole('ADMIN_ROLE'),createReservation);
-router.get('/', getReservations);
-router.get('/:id', getReservationById);
-router.put('/update/:id',validateJWT,
-    requireRole('ADMIN_ROLE'), updateReservation);
-router.delete('/delete/:id',validateJWT,
-    requireRole('ADMIN_ROLE'), deleteReservation);
+router.post(
+    '/create', validateJWT,
+    requireRole('ADMIN_ROLE'), createReservation
+);
+router.get(
+    '/', getReservations
+);
+router.get(
+    '/:id', getReservationById
+);
+router.put(
+    '/update/:id',validateJWT,
+    requireRole('ADMIN_ROLE'), updateReservation
+);
+router.delete(
+    '/delete/:id',validateJWT,
+    requireRole('ADMIN_ROLE'), deleteReservation
+);
 
 export default router;
