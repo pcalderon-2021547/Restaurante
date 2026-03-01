164856691470b8976cefa1e8a2d5fb9c564f60fe
pdeleon-2021364 <pdeleon-2021364@kinal.edu.gt>
2026-03-01 17:05:50 -0600

    feat: se implementa un middleware para el control de errores de la entidad order

--- src/fields/order/orderRoutes.js
+++ src/fields/order/orderRoutes.js
@@ -1,16 +1,49 @@
 import { Router } from 'express';
-import {createOrder,getOrders,updateOrder,deleteOrder} from './orderController.js';
+import {
+    createOrder,
+    getOrders,
+    updateOrder,
+    deleteOrder
+} from './orderController.js';
+
 import { validateJWT } from '../../../middlewares/validate_jwt.js';
 import { requireRole } from '../../../middlewares/validate_role.js';
 
+import {
+    validateOrderId,
+    validateCreateOrder,
+    validateOrderStatus
+} from '../../../middlewares/orderValidator.js';
+
 const router = Router();
 
-router.post('/create',validateJWT,
-    requireRole('ADMIN_ROLE'), createOrder);
+router.post(
+    '/create',
+    validateJWT,
+    requireRole('ADMIN_ROLE'),
+    validateCreateOrder,
+    createOrder
+);
+
+
 router.get('/', getOrders);
-router.put('/update/:id',validateJWT,
-    requireRole('ADMIN_ROLE'), updateOrder);
-router.delete('/delete/:id',validateJWT,
-    requireRole('ADMIN_ROLE'), deleteOrder);
+
+
+router.put(
+    '/update/:id',
+    validateJWT,
+    requireRole('ADMIN_ROLE'),
+    validateOrderId,
+    validateOrderStatus,
+    updateOrder
+);
+
+router.delete(
+    '/delete/:id',
+    validateJWT,
+    requireRole('ADMIN_ROLE'),
+    validateOrderId,
+    deleteOrder
+);
 
 export default router;
