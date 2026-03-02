346f30b15f2aa6f19dd7f9652184fa5323472dfe
pcalderon-2021547 <pcalderon-2021547@kinal.edu.gt>
2026-03-02 11:49:39 -0600

    feat: eliminacion de middelwar con error

--- src/fields/reservation/reservationRoutes.js
+++ src/fields/reservation/reservationRoutes.js
@@ -1,7 +1,6 @@
 import { Router } from 'express';
 import {createReservation,getReservations,getReservationById,updateReservation,cancelReservation,getReservationsByDate, getMyReservations } from './reservationController.js';
 import { validateJWT } from '../../../middlewares/validate_jwt.js';
-import { validateCreateReview, normalizeReviewOwnership } from '../../../middlewares/reservationAndReviewValidator.js';
 
 
 
@@ -10,7 +9,6 @@ const router = Router();
 //crear
 router.post(
     '/create', validateJWT,
-    validateCreateReview,
     createReservation
 );
 //listar
@@ -30,13 +28,11 @@ router.get(
 //actualizar
 router.put(
     '/update/:id',validateJWT,
-    normalizeReviewOwnership,
      updateReservation
 );
 //eliminar
 router.delete(
     '/delete/:id',validateJWT,
-    normalizeReviewOwnership,
     cancelReservation
 );
 
